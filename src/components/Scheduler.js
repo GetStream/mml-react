52a720270339b08b0c70200a7d8fa49640f87355
Guyon Morée <guyon.moree@gmail.com>
2020-09-03 17:42:56 +0200

    first attempt at modernizing the scheduler component

--- src/components/Scheduler.js
+++ src/components/Scheduler.js
@@ -6,12 +6,51 @@ import DatePicker from 'react-datepicker'
 /**
  * Scheduler datepicker element.
  */
+const setupIcalFilter = async (icalURL, duration, setLoading, setError) => {
+  let icalExpander = null
+
+  if (!icalURL) {
+    setLoading(false)
+  } else {
+    setLoading(true)
+    const response = await fetch(icalURL, {
+      method: 'GET',
+      redirect: 'follow'
+    })
+    const bodyText = await response.text()
+    if (response.ok) {
+      const options = { ics: bodyText, maxIterations: 10 }
+      icalExpander = new IcalExpander(options)
+    } else {
+      setError('failed to load availability')
+    }
+    setLoading(false)
+  }
+
+  return start => {
+    if (icalExpander) {
+      const stop = new Date(start.getTime() + duration * 60000)
+      const events = icalExpander.between(start, stop)
+
+      const booked = events && events.length >= 1
+      return !booked
+    } else {
+      return true
+    }
+  }
+}
+
 export function Scheduler({ name, ...props }) {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
 
   const mmlContext = useContext(MMLContext)
   const value = mmlContext[name]
+
+  const duration = props.duration ? 1 * parseInt(props.duration, 10) : 30
+
+  let icalFilter
+  // let icalFilter = setupIcalFilter(props.ical_availability, duration, setLoading, setError)
 
   const dateOnly = !!props.full_day
   const interval = props.interval ? 1 * props.interval : 30
@@ -31,7 +70,7 @@ export function Scheduler({ name, ...props }) {
           timeIntervals={interval}
           showTimeSelect={!dateOnly}
           dateFormat={format}
-          // filterDate={this.icalFilter}
+          filterDate={icalFilter}
           timeCaption="Time"
           popperPlacement="top-end"
           popperModifiers={{
