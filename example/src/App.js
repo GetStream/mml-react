2c3c796b3f4c1f65ebc678232d9af3690bf73b42
kuus <kunderikuus@gmail.com>
2020-10-16 14:30:01 +0200

    forgot unit

--- example/src/App.js
+++ example/src/App.js
@@ -16,7 +16,7 @@ import 'stream-chat-react/dist/css/index.css'
 const Wrap = styled.div`
   flex: 1;
   width: 100%;
-  max-width: ${theme.containerMax + theme.gutter * 10};
+  max-width: ${theme.containerMax + theme.gutter * 10}px;
   margin: 0 auto;
   background: white;
 `
