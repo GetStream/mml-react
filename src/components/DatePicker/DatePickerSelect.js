aca621a0bfa68433b6faf22953761c69cabf684d
Amin Mahboubi <aminmahboobi@gmail.com>
2020-11-12 12:58:25 +0100

    cleanup

--- src/components/DatePicker/DatePickerSelect.js
+++ src/components/DatePicker/DatePickerSelect.js
@@ -1,7 +1,23 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Virtuoso } from 'react-virtuoso';
-import Scroller from '../Scroller';
+
+/**
+ * Custom scroll container to hide scrollbar with simple css
+ */
+function Scroller({ className, style, reportScrollTop, scrollTo, children }) {
+  const elRef = React.useRef(null);
+
+  scrollTo((scrollTop) => {
+    elRef.current.scrollTo({ top: scrollTop });
+  });
+
+  return (
+    <div className="mml-scroller" ref={elRef} onScroll={(e) => reportScrollTop(e.target.scrollTop)} tabIndex={0}>
+      <div className="mml-scroller__inner">{children}</div>
+    </div>
+  );
+}
 
 /**
  * DatePicker select
