f87f2d4d665ab60bb720f37de870391408618444
Amin Mahboubi <amin@getstream.io>
2020-11-19 12:13:16 +0100

    Update jest-global-setup.js

--- jest-global-setup.js
+++ jest-global-setup.js
@@ -1,16 +1,3 @@
-// import { v4 as uuid } from 'uuid';
-
-// jest.mock('uuid');
-
-// describe('mock uuid', () => {
-//   it('should return testid }', () => {
-//     uuid.mockImplementation(() => '00000000-0000-0000-0000-000000000000');
-//   });  
-// });
-
-// @see https://github.com/facebook/jest/issues/2172
-// jest.mock('uuid', () => () => '00000000-0000-0000-0000-000000000000');
-
 /* eslint-disable require-await */
 module.exports = async () => {
   process.env.TZ = 'UTC';
