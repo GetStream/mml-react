77fa6b5a6534a3947f13b26bc73372923983a2b0
Amin Mahboubi <aminmahboobi@gmail.com>
2020-11-23 16:21:52 +0100

    update docs

--- docs/styles-0278046482c71956d358.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[33],[]]);
-//# sourceMappingURL=styles-0278046482c71956d358.js.map
--- docs/styles-f8ea374dc4c63ac9a8c7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[32],[]]);
-//# sourceMappingURL=styles-f8ea374dc4c63ac9a8c7.js.map
