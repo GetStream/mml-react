67ee4ad8a8525d94b6d7450e8c9516c56497a744
kuus <kunderikuus@gmail.com>
2020-11-25 09:39:40 +0100

    add scss file imports in index so that rollup pick them up

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import './styles/index.scss';
+import './styles/index-dark.scss';
+import './styles/commerce.scss';
+import './styles/commerce-dark.scss';
+import './styles/livestream.scss';
+import './styles/livestream-dark.scss';
+import './styles/messaging.scss';
+import './styles/messaging-dark.scss';
 import './styles/team.scss';
+import './styles/team-dark.scss';
 
 export * from './mml';
 export * from './parser';
