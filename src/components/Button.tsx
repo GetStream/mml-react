397b15e81541598d838b9b2a68baf9ec0b63ae7b
kuus <kunderikuus@gmail.com>
2020-11-26 09:43:14 +0100

    add button icon prop with examples and tests

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,11 @@
 import React, { FC, SyntheticEvent } from 'react';
 import { sanitizeUrl } from '@braintree/sanitize-url';
 
+import { Icon } from './Icon';
+
 export type ButtonProps = {
+  /** Additional button class name */
+  className?: string;
   /** The text to display in the button */
   text: string;
   /** The name of the button */
@@ -12,16 +16,27 @@ export type ButtonProps = {
   url?: string;
   /** Button style variant */
   variant?: 'floating';
+  /** Optional button icon name to display besides the text (from [material icons](https://material.io/resources/icons/)) */
+  icon?: string;
 };
 
 /**
  * A simple Button
  */
-export const Button: FC<ButtonProps> = ({ text, name, value, url = '', variant }) => {
+export const Button: FC<ButtonProps> = ({ className = '', text, name, value, url = '', variant, icon }) => {
   // TODO: fix submit handler for Button list
+  if (icon) {
+    className += text ? ' mml-btn--with-icon' : ' mml-btn--icon';
+  } else {
+    className += ' mml-btn--text';
+  }
+  if (variant === 'floating') {
+    className += ' mml--floating';
+  }
+
   return (
     <button
-      className={`mml-btn ${variant === 'floating' ? ' mml--floating' : ''}`}
+      className={`mml-btn ${className}`}
       type="submit"
       name={name}
       value={value}
@@ -32,6 +47,7 @@ export const Button: FC<ButtonProps> = ({ text, name, value, url = '', variant }
         }
       }}
     >
+      {icon && <Icon name={icon} />}
       {text}
     </button>
   );
