c432f66236ebc952ba32ec144f0d86873ea74d53
kuus <kunderikuus@gmail.com>
2020-11-26 16:09:48 +0100

    change number classnames for correct styling

--- src/components/Number.tsx
+++ src/components/Number.tsx
@@ -17,12 +17,11 @@ export const Number: FC<NumberProps> = ({ name, value }) => {
   return (
     <div className="mml-number">
       <input name={name} value={state} type="hidden" />
-
-      <span className="mml-number__btn mml-number__btn--dec" onClick={() => setState(state - 1)}>
+      <span className="mml-btn mml--floating mml-btn--icon mml-number__dec" onClick={() => setState(state - 1)}>
         <Icon name="remove" />
       </span>
       <span className="mml-number__count">{state}</span>
-      <span className="mml-number__btn mml-number__btn--inc" onClick={() => setState(state + 1)}>
+      <span className="mml-btn mml--floating mml-btn--icon mml-number__inc" onClick={() => setState(state + 1)}>
         <Icon name="add" />
       </span>
     </div>
