623077301dc8ed69ce345e5e5444d7da2c34792d
kuus <kunderikuus@gmail.com>
2020-12-02 15:02:47 +0100

    input: add label prop (reuses card-header classes

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,6 +3,8 @@ import React, { FC, useState } from 'react';
 export type InputProps = {
   /** The name of the input */
   name: string;
+  /** The label of the input (it is presented as a `CardHeader` */
+  label?: string;
   /** Initial value of the input */
   value?: string;
   /** The placeholder of the input field */
@@ -12,16 +14,25 @@ export type InputProps = {
 /**
  * Text input element. Usually you'll want to rely on regular messages
  */
-export const Input: FC<InputProps> = ({ name, value = '', placeholder = '' }) => {
+export const Input: FC<InputProps> = ({ name, label, value = '', placeholder = '' }) => {
   const [state, setState] = useState(value);
+  const id = `mml-${name}`;
 
   return (
-    <input
-      className="mml-input"
-      name={name}
-      value={state}
-      placeholder={placeholder}
-      onChange={(event) => setState(event.target.value)}
-    />
+    <>
+      {label && (
+        <label className="mml-card-header" htmlFor={id}>
+          <span className="mml-card-header__text">{label}</span>
+        </label>
+      )}
+      <input
+        id={id}
+        className="mml-input"
+        name={name}
+        value={state}
+        placeholder={placeholder}
+        onChange={(event) => setState(event.target.value)}
+      />
+    </>
   );
 };
