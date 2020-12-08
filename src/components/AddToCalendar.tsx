6c0d4a3edda4a9b7ea8d3976881fe057cd614523
Amin Mahboubi <aminmahboobi@gmail.com>
2020-12-08 21:22:00 +0100

    update deps

--- src/components/AddToCalendar.tsx
+++ src/components/AddToCalendar.tsx
@@ -1,6 +1,5 @@
 import React, { FC, ReactElement, SyntheticEvent } from 'react';
 import dayjs from 'dayjs';
-import isMobile from 'is-mobile';
 
 import { Card } from './Card';
 import { CardHeader } from './CardHeader';
@@ -39,6 +38,16 @@ const IconMicrosoft = (
 const IconApple = (
   <SvgIcon path="M18.7 19.5c-.83 1.24-1.7 2.45-3.05 2.47-1.34.03-1.77-.8-3.3-.8-1.53 0-2 .77-3.27.82-1.3.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.4c.87-1.52 2.43-2.48 4.12-2.5 1.28-.02 2.5.87 3.3.87.78 0 2.26-1.07 3.8-.9.65.03 2.47.26 3.64 1.98-.1.06-2.17 1.28-2.15 3.8.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.2-.7.85-1.83 1.5-2.95 1.42-.15-1.15.4-2.35 1.05-3.1z" />
 );
+
+function isMobile() {
+  const ua = navigator?.userAgent;
+  if (!ua) return false;
+  const mobileRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i;
+  if (mobileRE.test(ua)) return true;
+  if (navigator && navigator.maxTouchPoints > 1 && ua.indexOf('Macintosh') !== -1 && ua.indexOf('Safari') !== -1)
+    return true;
+  return false;
+}
 
 const isIE = (typeof window !== 'undefined' && window.navigator.msSaveOrOpenBlob && window.Blob) as boolean;
 
