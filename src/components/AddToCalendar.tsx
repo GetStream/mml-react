0a75d25fa603b0fbc643c590529eac211c003fb0
Amin Mahboubi <aminmahboobi@gmail.com>
2020-12-08 21:30:39 +0100

    undef check

--- src/components/AddToCalendar.tsx
+++ src/components/AddToCalendar.tsx
@@ -40,11 +40,16 @@ const IconApple = (
 );
 
 function isMobile() {
-  const ua = navigator?.userAgent;
+  const ua = typeof navigator !== 'undefined' ? navigator.userAgent : '';
   if (!ua) return false;
   const mobileRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i;
   if (mobileRE.test(ua)) return true;
-  if (navigator && navigator.maxTouchPoints > 1 && ua.indexOf('Macintosh') !== -1 && ua.indexOf('Safari') !== -1)
+  if (
+    typeof navigator !== 'undefined' &&
+    navigator.maxTouchPoints > 1 &&
+    ua.indexOf('Macintosh') !== -1 &&
+    ua.indexOf('Safari') !== -1
+  )
     return true;
   return false;
 }
