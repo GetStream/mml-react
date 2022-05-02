35bafce11c19449385f7f62596e809d7ce337700
MartinCupela <martin.cupela@gmail.com>
2022-05-02 12:21:24 +0200

    refactor: use PropsWithChildren instead of FC

--- src/components/CarouselItem.tsx
+++ src/components/CarouselItem.tsx
@@ -1,11 +1,7 @@
-import React, { FC, ReactNode } from 'react';
+import React, { PropsWithChildren } from 'react';
 import { CarouselProps } from './Carousel';
 
 export type CarouselItemProps = {
-  /**
-   * The carousel item inner components can contain any type of nodes, such as buttons, images, etc.
-   */
-  children?: ReactNode;
   /**
    * Determines the slide width, it can be set to either a percentage, e.g. `width="40%"` or to a pixel based value `width="200px"`.
    * It defaults to the `slideWidth` prop set at the `<carousel>` level
@@ -22,7 +18,12 @@ export type CarouselItemProps = {
 /**
  * A carousel item
  */
-export const CarouselItem: FC<CarouselItemProps> = ({ children, slideWidth, width = '', className = '' }) => {
+export const CarouselItem = ({
+  children,
+  slideWidth,
+  width = '',
+  className = '',
+}: PropsWithChildren<CarouselItemProps>) => {
   const finalWidth = width || slideWidth;
   return (
     <div className={`mml-carousel-item ${className}`} style={{ flex: `0 0 ${finalWidth}`, minWidth: finalWidth }}>
